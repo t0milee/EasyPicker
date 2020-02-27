8fca93cfe80b83de62e0c4c68495ffc8f531f82a
Tomislav Jankovic <jankovic.tm@gmail.com>
2020-02-27 15:06:54 +0100

    added files in order for the podspec to pass linting

---
+++ EasyPicker/EasyDatePicker/EasyDatePicker.swift
@@ -0,0 +1,13 @@
+//
+//  EasyDatePicker.swift
+//  EasyPicker
+//
+//  Created by Tomislav Jankovic on 27/02/2020.
+//  Copyright © 2020 TomislavJankovic. All rights reserved.
+//
+
+import UIKit
+
+class EasyDatePicker: NSObject {
+
+}
---
+++ EasyPicker/EasyStringPicker/EasyStringPicker.swift
@@ -0,0 +1,13 @@
+//
+//  EasyStringPicker.swift
+//  EasyPicker
+//
+//  Created by Tomislav Jankovic on 27/02/2020.
+//  Copyright © 2020 TomislavJankovic. All rights reserved.
+//
+
+import UIKit
+
+class EasyStringPicker: NSObject {
+
+}
