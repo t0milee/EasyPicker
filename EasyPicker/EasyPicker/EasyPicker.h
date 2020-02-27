cb0d1f402902c13839649c17be21681a53d223b0
Tomislav Jankovic <jankovic.tm@gmail.com>
2020-02-27 14:04:13 +0100

    created framework project

---
+++ EasyPicker/EasyPicker/EasyPicker.h
@@ -0,0 +1,19 @@
+//
+//  EasyPicker.h
+//  EasyPicker
+//
+//  Created by Tomislav Jankovic on 27/02/2020.
+//  Copyright © 2020 TomislavJankovic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EasyPicker.
+FOUNDATION_EXPORT double EasyPickerVersionNumber;
+
+//! Project version string for EasyPicker.
+FOUNDATION_EXPORT const unsigned char EasyPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyPicker/PublicHeader.h>
+
+
---
+++ EasyPicker/EasyPickerTests/EasyPickerTests.swift
@@ -0,0 +1,34 @@
+//
+//  EasyPickerTests.swift
+//  EasyPickerTests
+//
+//  Created by Tomislav Jankovic on 27/02/2020.
+//  Copyright © 2020 TomislavJankovic. All rights reserved.
+//
+
+import XCTest
+@testable import EasyPicker
+
+class EasyPickerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
